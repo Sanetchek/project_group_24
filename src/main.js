e0c2bff3fac37505af5fee7f2335c91cc5bc07b1
Aleksandr <algryshko@gmail.com>
2024-02-11 23:45:01 +0200

    add preloader

--- src/main.js
+++ src/main.js
@@ -99,3 +99,8 @@ document.addEventListener('DOMContentLoaded', function () {
   // AOS Init
   AOS.init();
 });
+
+window.addEventListener('load', function() {
+  var preloader = document.querySelector('.preloader');
+  preloader.style.display = 'none';
+});
